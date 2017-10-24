652fc252a4f70e68919628c31cfe1e4d94213e68
returnfortheking <31547179+returnfortheking@users.noreply.github.com>
2017-10-24 17:31:47 +0800

    day3
    小改

--- lintCode/LongestCommonPrefix.java
+++ lintCode/LongestCommonPrefix.java
@@ -1,9 +1,26 @@
+/*
+ * author:ReturnOfTheKing
+ * 
+ * 最长公共前缀
+ * 给k个字符串，求出他们的最长公共前缀(LCP)
+ * 
+ * 样例
+ * 在 "ABCD" "ABEF" 和 "ACEF" 中,  LCP 为 "A"
+ * 在 "ABCDEFG", "ABCEFG", "ABCEFA" 中, LCP 为 "ABC"
+ * 
+ * 1.依次比较首字母，若相等累加到输出，若不等直接输出。很简单。
+ * 主要需要处理各种越界和空指针的情况。
+ */
 package lintCode;
 
 public class LongestCommonPrefix {
 	private String longestCommonPrefix(String[] strs) {
         // write your code here
         String s="";
+        if(strs.length==0||strs==null)
+        {
+            return s;
+        }
         int temp=strs[0].length();
         for(int i=1;i<strs.length;i++)
         {
@@ -14,7 +31,7 @@ public class LongestCommonPrefix {
         }
         for(int i=0;i<temp;i++)
         {
-        	for(int j=1;j<temp;j++)
+        	for(int j=1;j<strs.length;j++)
         	{
         		if(strs[0].charAt(i)!=strs[j].charAt(i))
         		{
@@ -27,7 +44,7 @@ public class LongestCommonPrefix {
     }
 	public static void main(String[] args) {
 		LongestCommonPrefix l=new LongestCommonPrefix();
-		String[] n={};
+		String[] n={"ABCDEFG", "ABCEFG", "ABCEFA"};
 		System.out.println(l.longestCommonPrefix(n));
 	}
 }
