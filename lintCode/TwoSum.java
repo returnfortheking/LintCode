dad23ea28cbbcc8ceade72baa1091c7816e3338e
returnfortheking <31547179+returnfortheking@users.noreply.github.com>
2017-10-27 23:42:26 +0800

    Day6
    第二个题只写出了n^2，明天尝试实现n和logn

--- lintCode/TwoSum.java
+++ lintCode/TwoSum.java
@@ -6,45 +6,32 @@
  * 你需要实现的函数twoSum需要返回这两个数的下标, 并且第一个下标小于第二个下标。
  * 注意这里下标的范围是 1 到 n，不是以 0 开头。
  * 
- * 
+ * 1.建立一个长度相同的新数组，令新数组每个位置的值至等于target减旧数组相应位置的值。
+ * 依次比较新旧数组，有相同元素的两个位置即为所求。
  */
 package lintCode;
 
 public class TwoSum {
     public int[] twoSum(int[] numbers, int target) {
-        // write your code here
-    	int l=numbers.length-1;
-    	int f=0;
-    	if(l%2==0)
-    	{
-    		while(target!=(numbers[f]+numbers[l]))
-        	{
-        		if(target>(numbers[f]+numbers[l]))
-        		{
-        			f=(f+l)/2;
-        		}else if(target<(numbers[f]+numbers[l]))
-        		{
-        			l=(f+l)/2;
-        		}
-        	}
-    	}else{
-    		while(target!=(numbers[f]+numbers[l]))
-        	{
-        		if(target>(numbers[f]+numbers[l]))
-        		{
-        			f=(f+l)/2;
-        		}else if(target<(numbers[f]+numbers[l]))
-        		{
-        			l=(f+l+1)/2;
-        		}
-        	}
-    	}
-    	
-    	int B[]=new int[2];
-    	B[0]=f+1;
-    	B[1]=l+1;
-    	return B;
-        
+        // 时间复杂度n和nlogn尚待完成。
+//    	int[] B=new int[numbers.length];
+//    	for(int i=0;i<numbers.length;i++)
+//    	{
+//    		numbers[]=target-numbers[i];
+//    	}
+        int[] two=new int[2];
+        for(int i=0;i<numbers.length-1;i++)
+        {
+            for(int j=i+1;j<numbers.length;j++)
+            {
+                if(target==(numbers[i]+numbers[j]))
+                {
+                    two[0]=i+1;
+                    two[1]=j+1;
+                }
+            }
+        }
+        return two;
     }
 
 	public static void main(String[] args) {
